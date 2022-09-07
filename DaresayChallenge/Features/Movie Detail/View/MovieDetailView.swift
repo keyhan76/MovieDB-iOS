6b58af3853c9f87759ef34664bb5d9076333c29e
Keihan <keyhankam@icloud.com>
2022-09-07 13:59:10 +0200

    Designed movieDetailView using SwiftUI

--- DaresayChallenge/Features/Movie Detail/View/MovieDetailView.swift
+++ DaresayChallenge/Features/Movie Detail/View/MovieDetailView.swift
@@ -9,12 +9,83 @@ import SwiftUI
 
 struct MovieDetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { metrics in
+            VStack(spacing: 0) {
+                
+                ImageView()
+                    .frame(height: metrics.size.height * 0.35)
+                
+                DetailView()
+                
+            }
+        }
     }
 }
 
 struct MovieDetailView_Previews: PreviewProvider {
     static var previews: some View {
         MovieDetailView()
+    }
+}
+
+// MARK: - Detail View
+struct DetailView: View {
+    var body: some View {
+        ZStack {
+            Color(.white)
+                .cornerRadius(20)
+            
+            VStack(alignment: .center) {
+                
+                DescriptionView()
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Label("Add to Favorites", systemImage: "heart")
+                }
+                .buttonStyle(.bordered)
+                .controlSize(.large)
+                .tint(.blue)
+                
+                Spacer()
+            }
+            .padding(.leading)
+            .offset(y: 25)
+        }
+    }
+}
+
+// MARK: - Description View
+struct DescriptionView: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Title goes here")
+                    .font(.title)
+                
+                Text("Rating")
+                    .font(.callout)
+                
+                Text("Description")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+// MARK: - Image View
+struct ImageView: View {
+    var body: some View {
+        Image("")
+            .resizable()
+            .scaledToFill()
+            .background(.red)
+            .ignoresSafeArea()
     }
 }
