f70e7d4955aaacad698ea80c9f12d0c741229a68
Keihan <keyhankam@icloud.com>
2022-09-12 13:47:00 +0200

    Introduced CoreDataStoreable and CoreDataAPIProtocol to have a cleaner code base

--- DaresayChallenge/Utilities/Data/CoreDataAPI.swift
+++ DaresayChallenge/Utilities/Data/CoreDataAPI.swift
@@ -8,8 +8,21 @@
 import Foundation
 import CoreData
 
+protocol CoreDataAPIProtocol {
+    var managedContext: NSManagedObjectContext { get }
+    var coreDataStore: CoreDataStore { get }
+    
+    func save() throws
+    func delete<T: NSManagedObject>(entity: T) throws
+    func fetchAllObjects<T: NSManagedObject>(entity: T.Type) throws -> [T]
+    func createManagedObject<T: NSManagedObject>(entity: T.Type) -> NSManagedObject
+    func fetchObject<T: NSManagedObject>(movieID: Int, entity: T.Type) throws -> [T]
+    func fetch<T: NSManagedObject>(entity: T.Type) throws -> NSFetchedResultsController<T>
+}
+
 final class CoreDataAPI {
     
+    // MARK: - Variables
     let managedContext: NSManagedObjectContext
     let coreDataStore: CoreDataStore
     
@@ -22,52 +35,6 @@ final class CoreDataAPI {
     
     // MARK: - Core Data Operations
     
-    public func createManagedObject<T: NSManagedObject>(entity: T.Type) -> NSManagedObject {
-        
-        let entity = generate(entity: entity)
-        let managedObject = NSManagedObject(entity: entity, insertInto: managedContext)
-        return managedObject
-    }
-    
-    public func save() throws {
-        coreDataStore.saveContext(managedContext)
-    }
-    
-    public func delete<T: NSManagedObject>(entity: T) throws {
-        managedContext.delete(entity)
-        coreDataStore.saveContext(managedContext)
-    }
-    
-    public func fetchAllObjects<T: NSManagedObject>(entity: T.Type) throws -> [T] {
-        let fetchRequest = T.fetchRequest()
-        
-        let objects = try managedContext.fetch(fetchRequest) as! [T]
-        return objects
-    }
-    
-    public func fetchObject<T: NSManagedObject>(movieID: Int, entity: T.Type) throws -> [T] {
-        let fetchRequest = T.fetchRequest()
-
-        fetchRequest.predicate = NSPredicate(format: "id == %i", movieID)
-        
-        let objects = try managedContext.fetch(fetchRequest) as! [T]
-        return objects
-    }
-    
-    public func fetch<T: NSManagedObject>(entity: T.Type) throws -> NSFetchedResultsController<T> {
-        
-        let entityName = String(describing: entity)
-        let request = NSFetchRequest<T>(entityName: entityName)
-        request.fetchBatchSize = 20
-        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-        
-        let fetchedResultsController = NSFetchedResultsController<T>(fetchRequest: request, managedObjectContext: managedContext, sectionNameKeyPath: nil, cacheName: nil)
-         
-        try fetchedResultsController.performFetch()
-        
-        return fetchedResultsController
-    }
-    
     private func generate<T: NSManagedObject>(entity: T.Type) -> NSEntityDescription {
         let entityName = String(describing: entity)
         
@@ -79,3 +46,50 @@ final class CoreDataAPI {
     }
 }
 
+// MARK: - CoreDataAPI Protocol
+extension CoreDataAPI: CoreDataAPIProtocol {
+    func createManagedObject<T: NSManagedObject>(entity: T.Type) -> NSManagedObject {
+        
+        let entity = generate(entity: entity)
+        let managedObject = NSManagedObject(entity: entity, insertInto: managedContext)
+        return managedObject
+    }
+    
+    func save() throws {
+        coreDataStore.saveContext(managedContext)
+    }
+    
+    func delete<T: NSManagedObject>(entity: T) throws {
+        managedContext.delete(entity)
+        coreDataStore.saveContext(managedContext)
+    }
+    
+    func fetchAllObjects<T: NSManagedObject>(entity: T.Type) throws -> [T] {
+        let fetchRequest = T.fetchRequest()
+        
+        let objects = try managedContext.fetch(fetchRequest) as! [T]
+        return objects
+    }
+    
+    func fetchObject<T: NSManagedObject>(movieID: Int, entity: T.Type) throws -> [T] {
+        let fetchRequest = T.fetchRequest()
+
+        fetchRequest.predicate = NSPredicate(format: "id == %i", movieID)
+        
+        let objects = try managedContext.fetch(fetchRequest) as! [T]
+        return objects
+    }
+    
+    func fetch<T: NSManagedObject>(entity: T.Type) throws -> NSFetchedResultsController<T> {
+        let entityName = String(describing: entity)
+        let request = NSFetchRequest<T>(entityName: entityName)
+        request.fetchBatchSize = 20
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        let fetchedResultsController = NSFetchedResultsController<T>(fetchRequest: request, managedObjectContext: managedContext, sectionNameKeyPath: nil, cacheName: nil)
+         
+        try fetchedResultsController.performFetch()
+        
+        return fetchedResultsController
+    }
+}
